68eed334e863f4cf66638408b922482c285f449f
ywlee861009 <ywlee861009@gmail.com>
2018-08-28 01:26:54 +0900

    (Android-chart) getchartdata 추가
    1. mainPresenter 에서 getChartData() 추가

--- MpAndroidChart/app/src/main/java/com/example/jestina/mpandroidchart/presenter/MainPresenter.java
+++ MpAndroidChart/app/src/main/java/com/example/jestina/mpandroidchart/presenter/MainPresenter.java
@@ -1,6 +1,10 @@
 package com.example.jestina.mpandroidchart.presenter;
 
 import com.example.jestina.mpandroidchart.contract.MainContract;
+import com.example.jestina.mpandroidchart.model.BarChartModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by JESTINA on 2018-08-27.
@@ -10,5 +14,20 @@ public class MainPresenter implements MainContract.Presenter {
     @Override
     public void initPresenter() {
 
+    }
+
+    public List<BarChartModel> getChartData() {
+        List<BarChartModel> barChartDataArr = new ArrayList<>();
+
+        for(int i=0; i<12; i++) {
+             barChartDataArr.add(
+                     new BarChartModel.Builder()
+                        .month(i)
+                        .setSellingAmount((int) (Math.random() * 1000))
+                        .build()
+            );
+        }
+
+        return barChartDataArr;
     }
 }
