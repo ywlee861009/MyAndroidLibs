ec0ca5dff146e6c302bb3fee31f524c62fdc8826
ywlee861009 <ywlee861009@gmail.com>
2018-08-29 21:48:00 +0900

    (Android-moveIntro) 애니메이션 추가

--- moveIntro/app/src/main/java/com/example/jestina/moveintro/MainActivity.java
+++ moveIntro/app/src/main/java/com/example/jestina/moveintro/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jestina.moveintro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mImvImage = (ImageView) findViewById(R.id.main_imv_images);
+
+        TranslateAnimation translateAnimation = new TranslateAnimation(0f, -500f, 0f, 0f);
+        translateAnimation.setDuration(5000);
+        translateAnimation.setRepeatCount(5);
+        translateAnimation.setRepeatMode(2);
+
+        mImvImage.setAnimation(translateAnimation);
     }
 }
