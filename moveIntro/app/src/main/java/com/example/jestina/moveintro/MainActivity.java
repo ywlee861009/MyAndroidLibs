fcf57614987b3daf927ca5bd801dce4791aa2e32
ywlee861009 <ywlee861009@gmail.com>
2018-08-29 21:39:42 +0900

    (Anroid-moveIntro) image 추가
    1. 이미지 추가

--- moveIntro/app/src/main/java/com/example/jestina/moveintro/MainActivity.java
+++ moveIntro/app/src/main/java/com/example/jestina/moveintro/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.jestina.moveintro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImvImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImvImage = (ImageView) findViewById(R.id.main_imv_images);
     }
 }
