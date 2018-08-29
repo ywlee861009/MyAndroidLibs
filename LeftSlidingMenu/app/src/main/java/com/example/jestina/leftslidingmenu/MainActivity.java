6fff3bced9cef682676b21c563bf7e70eb9747d2
ywlee861009 <ywlee861009@gmail.com>
2018-08-29 21:05:22 +0900

    (Android-slide) Snack bar action 추가
    1. 스낵바 액션 추가

--- LeftSlidingMenu/app/src/main/java/com/example/jestina/leftslidingmenu/MainActivity.java
+++ LeftSlidingMenu/app/src/main/java/com/example/jestina/leftslidingmenu/MainActivity.java
@@ -12,23 +12,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mTxvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTxvHello = (TextView) findViewById(R.id.main_txv_hello);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "SnackBar Test", Snackbar.LENGTH_LONG)
+                        .setAction("Click", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mTxvHello.setText("Why?");
+                            }
+                        }).show();
             }
         });
 
