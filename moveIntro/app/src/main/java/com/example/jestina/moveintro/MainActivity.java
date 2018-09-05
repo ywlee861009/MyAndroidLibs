befdecefa2432944844addf2b3c47db70d9c8b48
ywlee861009 <ywlee861009@gmail.com>
2018-09-06 07:02:31 +0900

    adf
    asdf

--- moveIntro/app/src/main/java/com/example/jestina/moveintro/MainActivity.java
+++ moveIntro/app/src/main/java/com/example/jestina/moveintro/MainActivity.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.bumptech.glide.request.target.Target;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
         mImvImage = (ImageView) findViewById(R.id.main_imv_images);
         mImvLogo = (ImageView) findViewById(R.id.main_imv_logo);
 
-        setImage();
-        setLogo();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 
@@ -40,12 +50,14 @@ public class MainActivity extends AppCompatActivity {
                 into(mImvLogo);
     }
 
+
+
     private void setImage() {
         int imageId = getImageResource();
 
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(mImvImage);
         Glide.with(this).
                 load(imageId).
-                animate(R.anim.left_to_right).
                 listener(new RequestListener<Integer, GlideDrawable>() {
                     @Override
                     public boolean onException(Exception e, Integer integer, Target<GlideDrawable> target, boolean b) {
@@ -54,13 +66,43 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public boolean onResourceReady(GlideDrawable glideDrawable, Integer integer, Target<GlideDrawable> target, boolean b, boolean b1) {
-                        YWLog.d("onResourceReady = " + glideDrawable.isAnimated());
+                        YWLog.d("onResourceReady = " + mImvImage.getWidth() + "/" + mImvImage.getHeight());
+
+                        TranslateAnimation translateAnimation = new TranslateAnimation(
+                                Animation.RELATIVE_TO_SELF, 0.0f,
+                                Animation.RELATIVE_TO_SELF, -1.0f,
+                                Animation.RELATIVE_TO_SELF, 0.0f,
+                                Animation.RELATIVE_TO_SELF, 0.0f
+                        );
+                        translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+                                YWLog.d("========> onAnimationStart() ");
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                YWLog.d("========> onAnimationEnd() ");
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+
+                            }
+                        });
+                        translateAnimation.setFillAfter(true);
+                        translateAnimation.setDuration(3000);
+
+                        mImvImage.setAnimation(translateAnimation);
+                        mImvImage.startAnimation(translateAnimation);
 
 
                         return false;
                     }
                 }).
-                into(mImvImage);
+                into(imageViewTarget);
+
+
     }
 
     @Override
@@ -69,8 +111,13 @@ public class MainActivity extends AppCompatActivity {
 
         // image view size
         YWLog.d("logoview Size = " + mImvLogo.getWidth() + "/" + mImvLogo.getHeight());
+        YWLog.d("imageview Size = " + mImvImage.getWidth() + "/" + mImvImage.getHeight());
         // screen size
         getScreenSize();
+
+
+        setImage();
+        setLogo();
 
     }
 
@@ -78,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         int imageResource = 0;
 
         String imageName = "img_" + String.valueOf((int) (Math.random() * 5) + 1);
-//        String imageName = "img_4";
+//        String imageName = "img";
 
         imageResource = getResources().getIdentifier(imageName, "drawable", getPackageName());
 
